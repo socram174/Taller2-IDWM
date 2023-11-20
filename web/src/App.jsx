bd5518dd3babe79854895825267ef5550ac1dd54
socram174 <socram174@hotmail.com>
2023-11-20 03:15:07 -0300

    feat: improved the responsiveness of the page and added a log out button to the navbar

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,19 +1,19 @@
-import { useState, useEffect, createContext } from 'react';
+import { useState, useEffect, createContext } from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Login from "./views/Login.jsx";
 import AdminPanel from "./views/AdminPanel.jsx";
-import './App.css';
-import Navbar from './components/Navbar.jsx';
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
 
 export const CurrentUserContext = createContext(null);
 
 function App() {
   const [message, setMessage] = useState("");
-    //localStorage.setItem("token", "Tom");
+  //localStorage.setItem("token", "Tom");
 
-    const isAuth = localStorage.getItem("token");
+  const isAuth = localStorage.getItem("token");
 
-    const [currentUser, setCurrentUser] = useState(isAuth);
+  const [currentUser, setCurrentUser] = useState(isAuth);
 
   // const getMessage = async () => {
   //   const response = await fetch("http://localhost:3000");
@@ -28,25 +28,31 @@ function App() {
 
   return (
     <>
-    <Navbar />
-      <BrowserRouter>
       <CurrentUserContext.Provider
-      value={{
-        currentUser,
-        setCurrentUser
-      }}
-    >
-        <Routes>
-          <Route path="/" element={isAuth ? <Navigate to="/home" />: <Login />} />
-          <Route
-            path="/home"
-            element={isAuth ? <AdminPanel /> : <Navigate to="/" />}
-          />
-        </Routes>
-        </CurrentUserContext.Provider>
-      </BrowserRouter>
+        value={{
+          currentUser,
+          setCurrentUser,
+        }}
+      >
+        
+        <BrowserRouter>
+        <Navbar isAuth={isAuth} />
+          <Routes>
+            <Route
+              path="/"
+              element={isAuth ? <Navigate to="/home" /> : <Login />}
+            />
+            <Route
+              path="/home"
+              element={isAuth ? <AdminPanel /> : <Navigate to="/" />}
+            />
+          </Routes>
+
+        </BrowserRouter>
+        
+      </CurrentUserContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,9 +1,36 @@
-const Navbar = () => {
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CurrentUserContext } from "../App";
+
+
+const Navbar = ({ isAuth }) => {
+
+    const { setCurrentUser } = useContext(CurrentUserContext);
+
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setCurrentUser(null);
+        navigate("/");
+    };
+
+  if (isAuth) {
     return (
-        <nav className="text-center bg-green-500 p-2  fixed w-full">
-            <h1 className="font-bold text-white text-4xl">DUMBO</h1>
-        </nav>
-    )
+      <nav className="text-center bg-green-500 p-2  fixed w-full flex justify-between items-center">
+        <h1 className="font-bold text-white text-4xl">DUMBO</h1>
+        <button onClick={logout} className="border-2 p-2 rounded-md bg-red-400 text-white font-bold hover:ring-2 hover:ring-green-500 hidden md:block">
+            Cerrar sesión
+        </button>
+      </nav>
+    );
+  }
+
+  return (
+    <nav className="text-center bg-green-500 p-2  fixed w-full">
+      <h1 className="font-bold text-white text-4xl">DUMBO</h1>
+    </nav>
+  );
 };
 
 export default Navbar;
